0d5763f2efc34e9c7c6c28c43a74f7f02bae233d
olegbc <drum70@mail.ru>
2015-01-31 02:27:45 +0200

    just before dropdowns

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,19 +11,21 @@ return array(
 
 	// application components
 	'components'=>array(
+	    /*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
+	    */
 		// uncomment the following to use a MySQL database
-		/*
+
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=test.ru;dbname=trackstar',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'pass@word1',
 			'charset' => 'utf8',
 		),
-		*/
+
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
--- protected/config/main.php
+++ protected/config/main.php
@@ -53,10 +53,10 @@ return array(
 		// uncomment the following to use a MySQL database
 
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=test.ru;dbname=trackstar',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'pass@word1',
 			'charset' => 'utf8',
 		),
 
